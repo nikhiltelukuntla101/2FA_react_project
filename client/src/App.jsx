197edbfb18e9b7df4ffddfa8cceb8885a38dff31
nikhiltelukuntla101 <nikhiltelukuntla999@gmail.com>
2026-03-08 23:41:17 +0530

    frontend setup done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+    </>
+  );
 }
 
 export default App;
---
+++ client/src/pages/Setup2FA.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Setup2FA = () => {
+  return <div>SetUp2FA</div>;
+};
+
+export default Setup2FA;
